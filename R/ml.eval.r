c9aeafb96fc565ffa45565867e5973f2bd31c7a4
Bernd Heesen <60698023+bheesen@users.noreply.github.com>
2022-12-15 17:25:11 +0100

    Add files via upload

--- R/ml.eval.r
+++ R/ml.eval.r
@@ -1,7 +1,7 @@
 #' Function to evaluate a machine learning model the correlation for numeric variables
 #'
 #' @param modell is the name of a machine learning model
-#' @param daten is a dataframe with data
+#' @param daten is a dataframe with data with variable x to be predicted
 #' @keywords machine learning, modeling, evaluation
 #' @return model name, dataframe name, rmse & rsq
 #' @example ml.eval(wfl.rg.lm.fit,autos.train)
@@ -11,10 +11,10 @@ ml.eval <- function(modell,daten) {
   daten.name <-deparse(substitute(daten))
   df<-modell %>%
     predict(daten) %>%
-    bind_cols(daten %>% select(Preis)) 
-  rmse<-yardstick::rmse(data=df,Preis,.pred) %>%
+    bind_cols(daten %>% select(x)) 
+  rmse<-yardstick::rmse(data=df,x,.pred) %>%
     select(-.estimator) 
-  rsq<-yardstick::rsq(data=df,Preis,.pred) %>%
+  rsq<-yardstick::rsq(data=df,x,.pred) %>%
     select(-.estimator)
   kennzahlen<-as.data.frame(rbind(rmse,rsq)) 
   df <- data.frame(Modell=modell.name,
